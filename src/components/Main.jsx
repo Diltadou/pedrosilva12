84d1fd84413659021322ba0e6c30a048c854df9e
Diltadou <kaikemoura072@gmail.com>
2024-04-02 22:20:32 -0300

    atividade marcelino
    gggggggg

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -3,33 +3,50 @@ import { useState } from "react";
 export default function Main(){
 
     const [nome, setNome] = useState("");
-    const [telefone, setTelefone] = useState("");
+    const [telefone, setTelefone] = useState();
+    const [listaContatos, setListaContatos] = useState ([]);
+    const salvar = function (){
+        if(nome == "" || telefone == ""){
+            console.log("O usuário não inseriu alguma das informações.");
+            Swal.fire({
+                title: "ERRO",
+                text: "Não foram inseridos algumas das informações!",
+                icon: "error"
+              });
+        }
+        else{
+            console.log("O nome do usuário é:", nome, "e seu telefone é:", telefone);
+            Swal.fire({
+                title: "Contato Salvo!",
+                text: "Seu contato foi salvo com sucesso!",
+                icon: "success"
+              });
+            registrar();
+        }
+    }
+    const registrar = () => {
+        console.table(listaContatos);
+        setListaContatos([listaContatos,
+        {
+            nomeSalvo: nome,
+            telefoneSalvo: telefone
+        }
+        ])
+    };
 
     return(
+        <div id="centralizacao-main">
         <main>
-            <form>
-                <label htmlFor="nome">Nome:</label>
-           <input
-           type="text"
-           name=""
-           id=""
-           onChange={(event)=> setNome(event.target.value)}
-           />
-           {nome}
-<br></br>
-<br></br>
-           <label htmlFor="telefone">Telefone:</label>
-           <input
-           type=""
-           name=""
-           id=""
-            onChange={(event) => setTelefone(event.target.value)}
-            />
-{telefone}
-<br></br>
-<br></br>
-        <button type="submit">Salvar</button> 
+            <div id="bloco-insersoes">
+        <form onSubmit={registrar}>
+            <label htmlFor="nome">Nome: </label>
+            <div className="padding-input"><input type="text" name="" id="nome" value={nome} placeholder="Seu Nome" onChange={(event)=> setNome(event.target.value)}/></div>
+            <label htmlFor="telefone">Telefone: </label>
+            <div className="padding-input"><input type="tel" id="telefone" value={telefone} placeholder="Seu Telefone" onChange={(event)=> setTelefone(event.target.value)}/></div>
+           <button type="button" onClick={salvar}>Enviar</button>
         </form>
+            </div>
         </main>
-    );
+        </div>
+    );
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,7 +1,9 @@
 export default function Header(){
     return(
         <header>
-            <h1>telefone</h1>
+            <div id="alinhar-header">
+            <p id="paragrafo-header"><ion-icon id="call-icon" name="call"></ion-icon>AGENDA TELEFÔNICA</p>
+            </div>
         </header>
-    )
+    );
 }
